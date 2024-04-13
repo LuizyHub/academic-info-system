e3ccd5b3c576862264fc447fd4018a66bdfdc015
luizy <luizy991212@gmail.com>
2024-04-14 02:22:38 +0900

    initial commit

---
+++ src/main/java/com/cse/academicinfosystem/AcademicInfoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cse.academicinfosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicInfoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicInfoSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cse/academicinfosystem/AcademicInfoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cse.academicinfosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicInfoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
